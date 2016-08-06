62e5277a5ad28bb1152cee2e7744904ab5aecc65
Grant Tremblay <granttremblay@gmail.com>
2016-08-06 17:08:57 -0400

    Added basic data table functionality. The code now parses the ACCEPT table, and can slice that table by a given cluster name, which the user supplies via the command line. The cluster name is flexible with regards to space and capitalization.

--- rainmaker/rainmaker.py
+++ rainmaker/rainmaker.py
@@ -3,20 +3,29 @@
 import sys, os
 import argparse
 import numpy as np
+from astropy.io import ascii
 
 
 def main():
     parser = argparse.ArgumentParser(description="It's gonna rain...",
-                                usage="python rainmaker.py -f data_table.txt")
+                                     usage="python rainmaker.py -f data_table.txt")
 
     parser.add_argument("-f", "--filename", dest="filename", required=True,
-                    help="input file with data", metavar="FILE",
-                    type=lambda x: is_valid_file(parser, x))
+                        help="input file with data", metavar="FILE",
+                        type=lambda x: is_valid_file(parser, x))
+
+    parser.add_argument("-n", "--name_of_cluster", dest="name_of_cluster", required=False,
+                        default='ABELL_2597', help="Name of the cluster, as written in the table")
+
     args = parser.parse_args()
     filename = args.filename.name
 
-    dirtytable = parse_data_table(filename)
-    print(dirtytable)
+    # Be flexible with the cluster name. If they entered a space,
+    # replace it with a '_', then convert it to UPPERCASE (as required by the ACCEPT table)
+    clustername = args.name_of_cluster.replace(" ", "_").upper()
+
+    data = parse_data_table(filename, clustername)
+    print(data)
     
 
 
@@ -25,12 +34,23 @@ def is_valid_file(parser, arg):
         parser.error("Cannot find that data table: %s" % arg)
     else:
         print("Successfully found input file: %s" % arg)
-        return open(arg, 'r')  # return an open file handle
+        return open(arg, 'r')      # return an open file handle
 
-def parse_data_table(intable):
-    a = np.genfromtxt(intable, names=True)
-    return a
 
+
+
+def parse_data_table(intable, cluster_name):
+    data = ascii.read(intable)     # This creates an Astropy TABLE object
+                                   # http://docs.astropy.org/en/v1.2.1/table/index.html#astropy-table
+
+    mask = data['Name'] == cluster_name
+    
+    # Check if that mask is an array of only FALSE, which means the name wasn't found
+    if not any(mask):
+        sys.exit("... but the cluster name was not found in the data table.")
+    else: 
+        masked_data = data[mask]
+        return masked_data
 
 
 def alive():
