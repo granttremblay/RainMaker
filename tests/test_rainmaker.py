eea490602dcfe84c8949714a4eb3c0c7fe8825e5
Grant Tremblay <granttremblay@gmail.com>
2016-08-06 11:43:55 -0400

    yolo

--- tests/test_rainmaker.py
+++ tests/test_rainmaker.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 
 '''
-Basic unit testing for every rainmaker function. 
-Run this either with py.test or `python test_rainmaker.py`
-in the /tests/ directory. 
+===================================
+Basic unit testing for rainmaker
+===================================
+
+
+Usage:  `py.test` or `python test_rainmaker.py` in the /tests/ directory.
+
+:Author: Dr. Grant R. Tremblay (Yale University)
+:Date: August 2016 
 
 The test explicitly sets the PATH so that rainmaker
 can be properly imported. 
+
+http://docs.python-guide.org/en/latest/writing/structure/
 '''
 
 import unittest
-
 import sys,os
+
+# Set the path explicitly #
 sys.path.insert(0,os.path.abspath(__file__+"/../.."))
 from rainmaker import rainmaker
+
+
 
 class TestBasics(unittest.TestCase):
     '''Test basic functionality to ensure the code is alive'''
