63976b9d07a9b04a2a84b525537e329bb70f51c8
Grant Tremblay <granttremblay@gmail.com>
2016-08-06 10:43:01 -0400

    more stuff

--- tests/test_rainmaker.py
+++ tests/test_rainmaker.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+
+'''
+Basic unit testing for every rainmaker function. 
+Run this either with py.test or `python test_rainmaker.py`
+in the /tests/ directory. 
+
+The test explicitly sets the PATH so that rainmaker
+can be properly imported. 
+'''
+
 import unittest
 
 import sys,os
@@ -5,7 +16,7 @@ sys.path.insert(0,os.path.abspath(__file__+"/../.."))
 from rainmaker import rainmaker
 
 class TestBasics(unittest.TestCase):
-
+    '''Test basic functionality to ensure the code is alive'''
     def test_alive(self):
         response = rainmaker.alive()
         self.assertTrue(response == "I'm alive!")
