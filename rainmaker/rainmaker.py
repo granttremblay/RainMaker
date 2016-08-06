c31fd5c4b12e8a937ed9eba4fd7769d5c0108519
Grant Tremblay <granttremblay@gmail.com>
2016-08-06 17:26:59 -0400

    Finished for today. Moved argument parsing to its own function, which is cleaner.

--- rainmaker/rainmaker.py
+++ rainmaker/rainmaker.py
@@ -5,8 +5,11 @@ import argparse
 import numpy as np
 from astropy.io import ascii
 
-
 def main():
+    parse_arguments()
+
+
+def parse_arguments():
     parser = argparse.ArgumentParser(description="It's gonna rain...",
                                      usage="python rainmaker.py -f data_table.txt")
 
@@ -15,7 +18,7 @@ def main():
                         type=lambda x: is_valid_file(parser, x))
 
     parser.add_argument("-n", "--name_of_cluster", dest="name_of_cluster", required=False,
-                        default='ABELL_2597', help="Name of the cluster, as written in the table")
+                        default='ABELL_2597', help="Name of the cluster, e.g. 'Abell 2597', 'ZWICKY_2701', etc.")
 
     args = parser.parse_args()
     filename = args.filename.name
@@ -33,10 +36,8 @@ def is_valid_file(parser, arg):
     if not os.path.isfile(arg):
         parser.error("Cannot find that data table: %s" % arg)
     else:
-        print("Successfully found input file: %s" % arg)
+        print("\nSuccessfully found input file: %s" % arg)
         return open(arg, 'r')      # return an open file handle
-
-
 
 
 def parse_data_table(intable, cluster_name):
@@ -47,7 +48,8 @@ def parse_data_table(intable, cluster_name):
     
     # Check if that mask is an array of only FALSE, which means the name wasn't found
     if not any(mask):
-        sys.exit("... but the cluster name was not found in the data table.")
+        print("\n...but the cluster name was not found in the data table :( \n")
+        sys.exit("Look here for appropriate names: http://www.pa.msu.edu/astro/MC2/accept/ \n")
     else: 
         masked_data = data[mask]
         return masked_data
