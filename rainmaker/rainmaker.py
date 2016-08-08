5a0a8086a00fcef5f59b3820b7c6e9e6ae410091
Grant Tremblay <granttremblay@gmail.com>
2016-08-08 17:29:40 -0400

    minor tweak to the user query on cluster name. I now strip quote marks if the user entered them - was causing issues with matching, because it was searching for the quote marks!

--- rainmaker/rainmaker.py
+++ rainmaker/rainmaker.py
@@ -13,14 +13,21 @@ import os
 import argparse
 import numpy as np
 from astropy.io import ascii
+import astropy.units as u
 
 
 def main():
+    '''The main program runs the whole sequence.'''
+
+    # Parse command line arguments. Iterate with user if cluster not found.
     filename, cluster_name = parse_arguments()
 
+    # DATA is an astropy TABLE object,
+    # filtered to show all properties of a given cluster
+    # Can be split by e.g. data['Rin'], data['Mgrav'], etc.
     data = parse_data_table(filename, cluster_name)
 
-    print(data)
+    logTemp(data)
 
 
 def parse_arguments():
@@ -88,8 +95,11 @@ def filter_by_cluster(data, cluster_name):
 
     while cluster_found is False:
             new_cluster_name = input("Cluster [" + cluster_name + "] not found, try again: ")
-            cluster_name = str(new_cluster_name).replace(' ', '_').upper()
-            cluster_found = cluster_name in clusters_in_table['Name']        
+            if new_cluster_name.startswith('"') and new_cluster_name.endswith('"'):
+                cluster_name = new_cluster_name[1:-1].replace(' ', '_').upper()
+            else:
+                cluster_name = new_cluster_name.replace(' ', '_').upper()
+            cluster_found = cluster_name in clusters_in_table['Name']
 
     if cluster_found is True:
         print("Matched cluster name to one in the table: " + cluster_name)
@@ -98,6 +108,35 @@ def filter_by_cluster(data, cluster_name):
         print(masked_data)
         return masked_data
 
+
+def fit_polynomial(x, y, deg):
+    '''Fits a DEG-order polynomial in x, y space'''
+
+    print("Fitting order=%s polynomial to %s, %s" % (deg, x, y))
+    coeffs = np.polyfit(x, y, deg)
+
+
+
+
+def logTemp(data):
+    ''' 
+    Fit the logarithmic electron density profile ln(n_e) (in cm^-3)
+    to a polynomial in log r (in Mpc) of degree 'deg'.
+    The array 'coeffs' returns the coefficients of that polynomial fit.
+    '''
+
+    nbins = len(data['Rin'])
+    r = (data['Rin'] + data['Rout']) * 0.5
+    logr = np.log10(r)
+
+    lognelec = np.log10(data['nelec'])
+    logneerr = np.log10(data['neerr'] / data['nelec'])
+    yerror = logneerr
+
+    deg = 3
+    coeffs = fit_polynomial(logr, lognelec, deg)
+
+    return coeffs
 
 def alive():
     response = "I'm alive!"
