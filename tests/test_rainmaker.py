325ee7fb617a481b3769837e371925381d51cab7
Grant Tremblay <granttremblay@gmail.com>
2017-02-16 14:29:42 -0500

    test

--- tests/test_rainmaker.py
+++ tests/test_rainmaker.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+import os
+import sys
+from astropy.io import ascii
+import astropy.units as u
+import astropy.constants as const
+
+import numpy as np
+
+import unittest
+
+# Set the path explicitly #
+sys.path.insert(0, os.path.abspath(__file__+"/../.."))
+from rainmaker import rainmaker
+
+
 '''
 ===================================
 Basic unit testing for rainmaker
@@ -17,27 +32,12 @@ can be properly imported.
 http://docs.python-guide.org/en/latest/writing/structure/
 '''
 
-import os
-import sys
-from astropy.io import ascii
-import astropy.units as u
-import astropy.constants as const
-
-import numpy as np
-
-import unittest
-
-# Set the path explicitly #
-sys.path.insert(0, os.path.abspath(__file__+"/../.."))
-from rainmaker import rainmaker
-
-
 
 class TestBasics(unittest.TestCase):
     '''Test basic functionality to ensure the code is alive'''
 
     def test_parse_data_table(self):
-        filename = os.getcwd() + "/testdata/accept_main_table.txt"
+        filename = "accept_main_table.txt"
         cluster_name = "ABELL_2597"
 
         returned_data = rainmaker.parse_data_table(filename, cluster_name)
@@ -46,7 +46,7 @@ class TestBasics(unittest.TestCase):
     def test_filter_by_cluster(self):
 
         '''Filtering by cluster name should work'''
-        filename = os.getcwd() + "/testdata/accept_main_table.txt"
+        filename = "accept_main_table.txt"
         cluster_name = "ABELL_2597"
         data = ascii.read(filename)
 
@@ -56,21 +56,18 @@ class TestBasics(unittest.TestCase):
 
     def test_assign_units(self):
 
-        filename = os.getcwd() + "/testdata/accept_main_table.txt"
+        filename = "accept_main_table.txt"
         data = rainmaker.parse_data_table(filename, "ABELL_2597")
 
         massValue = data['Mgrav'][0]
         radiusValue = data['Rin'][0]
-
-        ismsol =  'solMass' is str(massValue.unit)
-        self.assertTrue(ismsol)
 
         Mpc_divides_correctly = radiusValue.unit / (u.pc * 1.0e6).to(u.Mpc)
         self.assertTrue(Mpc_divides_correctly == 1)
 
     def test_fit_polynomial(self):
 
-        filename = os.getcwd() + "/testdata/accept_main_table.txt"
+        filename = "accept_main_table.txt"
         cluster_name = "ABELL_2597"
         data = ascii.read(filename)
         data = rainmaker.parse_data_table(filename, "ABELL_2597")
